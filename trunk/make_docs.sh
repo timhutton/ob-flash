7707a8bef75d74a49a7ebdd47bdc2d1f65f1b33c
tim.hutton <tim.hutton@4d6fa177-144a-0410-a20a-976c5e22f1ab>
2008-05-08 00:23:40 +0000

    ADD: i18n framework, for runtime locale switching (first draft)

---
+++ trunk/make_docs.sh
@@ -0,0 +1,2 @@
+asdoc -doc-sources obClasses -output docs -package obClasses "Contains all the components specifically developed for this application."
+
---
+++ trunk/make_ob.sh
@@ -0,0 +1,1 @@
+mxmlc -locale=en_US,fr_FR -source-path=locale/{locale} -allow-source-path-overlap=true ob_flash.mxml
